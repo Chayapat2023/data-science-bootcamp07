e60bb5f60b12687153f9aa71a088a9521360e0fa
Chayapat Fonbamroer <142607285+Chayapat2023@users.noreply.github.com>
2025-07-20 21:06:37 +0700

    Update P1_Queries Quality and Percentage.sql

--- project03/P1_Queries Quality and Percentage.sql
+++ project03/P1_Queries Quality and Percentage.sql
@@ -2,5 +2,5 @@ SELECT
     query_name,
     ROUND(AVG(rating/position),2) as quality,
     ROUND(SUM(IF(rating <3,1,0))/COUNT(*)*100,2) as poor_query_percentage  
- FROM Queries
+ FROM P1_data
 GROUP BY query_name 
